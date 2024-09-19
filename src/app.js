3ab7b013e77bfbcda81a5f196cf233aadb7565d9
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-09-19 23:05:46 +0000

    refactorizacion

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  //write your code here
+  let who = ["The cat", "My sister", "The zombie", "My bird"];
+  let action = ["ate", "hid", "crushed", "broke", "stole"];
+  let what = ["my computer", "my phone", "the key", "the car"];
+  let when = [
+    "after the class",
+    "when I was eating",
+    "while I was sleeping",
+    "during my dinner",
+    "while I was laughing"
+  ];
+
+  const getRandomExcuse = arr => arr[Math.floor(Math.random() * arr.length)];
+  const generatorExcuse = () =>
+    `${getRandomExcuse(who)} ${getRandomExcuse(action)} ${getRandomExcuse(
+      what
+    )} ${getRandomExcuse(when)}`;
+
+  document.querySelector("#the_excuse").innerHTML = generatorExcuse();
 };
